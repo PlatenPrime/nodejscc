b8500d804715257589f4f389b9669a0d2b0fec8f
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-05-14 08:21:34 +0300

    14.05.2024

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-console.log(process);
+import {getPosts, getPostsLength} from "./postController.js";
+
+console.table(getPosts());
+console.log(getPostsLength());
+
+
+
+
+
+
+
+
+// const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+// console.log(`Random number between 1 and 100: ${generateRandomNumber()}`);
+// console.log(`Celsius to Fahrenheit: ${celciusToFahrenheit(40)}`);
+
+
 
 
 
