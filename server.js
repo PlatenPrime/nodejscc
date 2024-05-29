61e8aab87d59bff33cd44364b1a9f16b83f9e083
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-05-29 08:56:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,32 @@ import http from 'http';
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-    // res.setHeader("Content-Type", "text/html");
-    // res.statusCode = 404
-    console.log(req.url);
-    console.log(req.method);
-    
-    
-    
-    
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("<h1>Hello World</h1>");
+
+    try {
+        //check if GET request
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end("<h1>Home Page</h1>");
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end("<h1>About Page</h1>");
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end("<h1>Page Not Found</h1>");
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end("<h1>Bad Gateway Server Error</h1>");
+
+    }
+
+
 });
 
 
